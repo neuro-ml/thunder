08610ffc6ca5dd8e858d7bcb355790ffbe7ed6ee
Alexey Shevtsov <a.shevtsov@ira-labs.com>
2023-06-12 11:27:39 +0300

    slurm backend draft

--- thunder/backend.py
+++ thunder/backend.py
@@ -52,19 +52,50 @@ class Cli(Backend):
             return
 
         Parallel(backend='loky', n_jobs=config.n_workers)(
-            delayed(subprocess.check_call)(['thunder', 'start', str(experiment), node.name])
-            for node in nodes
+            delayed(subprocess.check_call)(['thunder', 'start', str(experiment), node.name]) for node in nodes
         )
 
 
 class Slurm(Backend):
     class Config(BackendConfig):
-        cpu: Annotated[float, Option(...)] = None
-        ram: Annotated[float, Option(...)] = None
+        mem: Annotated[float, Option(...)] = 16
+        cpus_per_task: Annotated[float, Option(...)] = 4
+        gpus_per_task: Annotated[float, Option(...)] = 0
+
+        job_name: Annotated[str, Option(...)] = None
+        partition: Annotated[str, Option(...)] = None
+        nodelist: Annotated[str, Option(...)] = None
 
     @staticmethod
     def run(config: Slurm.Config, experiment: Path, nodes: Optional[Sequence[Node]]):
-        raise NotImplementedError
+        args = ['sbatch']
+
+        args.extend(
+            [
+                f'--mem {int(config.mem)}',
+                f'--cpus-per-task {config.cpus_per_task}',
+                f'--gpus-per-node {config.gpus_per_task}',
+            ]
+        )
+
+        if config.job_name:
+            args.append(f'--job-name {config.job_name}')
+
+        if config.partition:
+            args.append(f'--partition {config.partition}')
+
+        if config.nodelist:
+            args.append(f'--nodelist {config.nodelist}')
+
+        args.extend(['#!/bin/bash\n', f'thunder start {str(experiment)}'])
+
+        if config.nodes is None:
+            subprocess.check_call(args)
+            return
+
+        Parallel(backend='loky', n_jobs=config.n_workers)(
+            delayed(subprocess.check_call)([*args, node.name]) for node in nodes
+        )
 
 
 backends = {
