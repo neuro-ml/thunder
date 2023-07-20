355546ce9ab63db67dd029238fc8d758411410d0
arseniybelkov <arsfizteh@gmail.com>
2023-07-20 21:57:54 +0300

    rework predictors

---
+++ tests/predict/test_predictors.py
@@ -0,0 +1,93 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from thunder.predict import Predictor, Map, Chain
+from thunder.torch.utils import maybe_from_np, to_np
+
+
+def predict(x: np.ndarray, model: nn.Module) -> np.ndarray:
+    model.eval()
+    with torch.no_grad():
+        x = maybe_from_np(x)
+        return to_np(model(x))
+
+
+def test_predictor_single_object():
+    predictor = Predictor()
+    model = reinit(torch.nn.Linear(4, 1))
+    batch = [np.random.randn(1, 4).astype(np.float32) for _ in range(8)]
+
+    for x in batch:
+        stream = predictor(x)
+        y = stream.send(predict(next(stream), model))
+
+        assert y.shape == (1, 1)
+        assert np.allclose(y, np.zeros_like(y))
+
+
+class SplitLinear(nn.Module):
+    def __init__(self, single_mode: bool):
+        super().__init__()
+        self.l1 = nn.Linear(4, 1)
+        self.l2 = nn.Linear(4, 1)
+        self.single_mode = single_mode
+
+    def forward(self, x) -> tuple:
+        if self.single_mode:
+            return self.l1(x), self.l2(x)
+        return self.l1(x[0]), self.l2(x[1])
+
+
+def test_predictor_multi_object():
+    predictor = Predictor()
+
+    model = reinit(SplitLinear(True))
+    batch = [np.random.randn(1, 4).astype(np.float32) for _ in range(8)]
+
+    for x in batch:
+        stream = predictor(x)
+        y = stream.send(predict(next(stream), model))
+
+        assert y[0].shape == (1, 1) and y[1].shape == (1, 1)
+        assert np.allclose(y, np.zeros_like(y))
+
+
+def test_map():
+    predictor = Map(Predictor())
+    model = reinit(torch.nn.Linear(4, 1))
+    batch = [np.random.randn(1, 4).astype(np.float32) for _ in range(8)]
+
+    stream = predictor(batch)
+    for x in stream:
+        y = stream.send(predict(x, model))
+        assert y.shape == (1, 1)
+        assert np.allclose(y, np.zeros_like(y))
+
+
+def test_chain():
+    class PlusOne(Predictor):
+        def backward(self, y):
+            return y + 1
+
+    predictor = Chain(Predictor(), PlusOne())
+
+    model = reinit(torch.nn.Linear(4, 1))
+    batch = [np.random.randn(1, 4).astype(np.float32) for _ in range(8)]
+
+    for x in batch:
+        stream = predictor(x)
+        y = stream.send(predict(next(stream), model))
+
+        assert y.shape == (1, 1)
+        assert np.allclose(y, np.ones_like(y))
+
+
+def reinit(model: nn.Module, constant: float = 0.0) -> nn.Module:
+    for m in model.modules():
+        if hasattr(m, "weight"):
+            m.weight.data.fill_(constant)
+        if hasattr(m, "bias"):
+            m.bias.data.fill_(constant)
+
+    return model
--- thunder/predict/predict.py
+++ thunder/predict/predict.py
@@ -1,94 +1,54 @@
-from abc import ABC, abstractmethod
-from itertools import chain
-from typing import Callable, Iterable
+from typing import Union, Tuple, Iterable, Any
 
-from more_itertools import zip_equal, mark_ends, chunked, split_after
-from toolz import compose
+import numpy as np
+import torch
+from toolz import compose, compose_left
 
-
-class BasePredictor(ABC):
-    """Base class for all predictors."""
-
-    @abstractmethod
-    def forward(self, batches: Iterable) -> Iterable:
-        """Process stream of batches before model inference."""
-        raise NotImplementedError("You must implement forward method")
-
-    @abstractmethod
-    def backward(self, predicts: Iterable) -> Iterable:
-        """Post-process stream of predictions."""
-        raise NotImplementedError("You must implement backward method")
-
-    def __call__(self, batches: Iterable, predict_fn: Callable) -> Iterable:
-        return self.run(batches, predict_fn)
-
-    def run(self, batches: Iterable, predict_fn: Callable) -> Iterable:
-        """Runs preprocessing, inference and postprocessing."""
-        return self.backward(map(predict_fn, self.forward(batches)))
+Array = Union[np.ndarray, torch.Tensor]
+ArrayLike = Union[Array, Tuple[Array, ...]]
 
 
-class InfinitePredictor(BasePredictor):
-    """Useful for running inference on infinite stream of data."""
+class BasePredictor:
+    def forward(self, x: ArrayLike) -> ArrayLike:
+        raise NotImplementedError("Method `forward` must be implemented.")
 
-    def forward(self, batches: Iterable) -> Iterable:
-        yield from batches
+    def backward(self, y: ArrayLike) -> Any:
+        raise NotImplementedError("Method `must` must be implemented.")
 
-    def backward(self, predicts: Iterable) -> Iterable:
-        yield from predicts
+    def __call__(self, *args, **kwargs) -> Iterable:
+        return self.run(*args, **kwargs)
 
-
-class Predictor(InfinitePredictor):
-    """Assumes using finite amount of data for inference to be run on."""
-
-    def run(self, batches: Iterable, predict_fn: Callable) -> Iterable:
-        return tuple(super().run(batches, predict_fn))
+    def run(self, x: ArrayLike) -> ArrayLike:
+        y = yield self.forward(x)
+        yield self.backward(y)
 
 
-class Decorated(InfinitePredictor):
-    """
-    Decorates inference function
-    Example
-    -----------
-    Decorated(f, g, h)
-    # inside Decorated
-    predict_fn = f(g(h(predict_fn)))
-    """
+class Predictor(BasePredictor):
+    def forward(self, x: ArrayLike) -> ArrayLike:
+        return x
 
-    def __init__(self, *decorators: Callable):
-        self.decorators = compose(*decorators)
-
-    def run(self, batches: Iterable, predict_fn: Callable) -> Iterable:
-        return super().run(batches, self.decorators(predict_fn))
+    def backward(self, y: ArrayLike) -> Any:
+        return y
 
 
-class SlidingWindow(BasePredictor):
-    def __init__(self, patcher: Callable, combiner: Callable, batch_size: int = 1, **patcher_kwargs):
+class Map(BasePredictor):
+    def __init__(self, predictor: BasePredictor):
         super().__init__()
-        self.patcher = patcher
-        self.batch_size = batch_size
-        self.combiner = combiner
-        self.patcher_kwargs = patcher_kwargs
+        self.predictor = predictor
 
-    def forward(self, batches: Iterable) -> Iterable:
-        recombined_batches = map(lambda batch: zip_equal(*batch), batches)
+    def run(self, xs: Iterable[ArrayLike]) -> Iterable[ArrayLike]:
+        for x in xs:
+            yield from self.predictor(x)
 
-        def get_patches(batches):
-            for xs in chain.from_iterable(batches):
-                patches = self.patcher(*xs, **self.patcher_kwargs)
-                yield from mark_ends(patches)
 
-        yield from chunked(get_patches(recombined_batches), self.batch_size)
+class Chain(BasePredictor):
+    def __init__(self, *predictors: BasePredictor):
+        super().__init__()
+        self._composed_forward = compose_left(*[p.forward for p in predictors])
+        self._composed_backward = compose(*[p.backward for p in predictors])
 
-    def backward(self, predicts: Iterable) -> Iterable:
-        patched_images = chain.from_iterable(map(lambda p: zip(*p), predicts))
-        for patched_image in split_after(patched_images, lambda pim: pim[1]):
-            yield self.combiner(patched_image)
+    def forward(self, x: ArrayLike) -> ArrayLike:
+        return self._composed_forward(x)
 
-    def run(self, batches: Iterable, predict_fn: Callable) -> Iterable:
-        def run_predict(batches, predict_fn):
-            for batch_of_patches in self.forward(batches):
-                firsts, lasts, patches = zip_equal(*batch_of_patches)
-                preds = predict_fn(patches)
-                yield firsts, lasts, preds
-
-        return self.backward(run_predict(batches, predict_fn))
+    def backward(self, y: ArrayLike) -> Any:
+        return self._composed_backward(y)
