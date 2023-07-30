23089bb58348e9c15bf67534c3b351e66a484378
arseniybelkov <arsfizteh@gmail.com>
2023-07-30 16:51:18 +0300

    force progress bar disabling

--- thunder/callbacks/inference_runner.py
+++ thunder/callbacks/inference_runner.py
@@ -1,8 +1,8 @@
 from typing import Callable, Sequence, Tuple, Union
 
 from lightning import LightningModule, Trainer
-from lightning.pytorch.callbacks import Callback
-from lightning.pytorch.trainer.call import _call_callback_hooks
+from lightning.pytorch.callbacks import Callback, TQDMProgressBar
+from lightning.pytorch.trainer.call import _call_callback_hooks, _call_lightning_module_hook
 from more_itertools import zip_equal
 from toolz import compose
 
@@ -14,11 +14,11 @@ Loader = Tuple[Sequence, Callable, Callable]
 
 class InferenceRunner(Callback):
     def __init__(
-            self,
-            *decorators: Callable,
-            val_loaders: Union[Loader, Sequence[Loader]] = None,
-            test_loaders: Union[Loader, Sequence[Loader]] = None,
-            predict_loaders: Union[Loader, Sequence[Loader]] = None
+        self,
+        *decorators: Callable,
+        val_loaders: Union[Loader, Sequence[Loader]] = None,
+        test_loaders: Union[Loader, Sequence[Loader]] = None,
+        predict_loaders: Union[Loader, Sequence[Loader]] = None,
     ):
         """
         Run inference on different stages, allowing you to get individual metrics.
@@ -30,6 +30,7 @@ class InferenceRunner(Callback):
         test_loaders : Union[Loader, Sequence[Loader]]
         predict_loaders : Union[Loader, Sequence[Loader]]
         """
+
         def _wrap_loader(loader):
             if len(loader) == 3:
                 return [loader] if callable(loader[1]) else loader
@@ -48,29 +49,25 @@ class InferenceRunner(Callback):
             return pl_module(*maybe_from_np(x, device=pl_module.device))
 
         self._predict = predict
+        trainer.callbacks = [c for c in trainer.callbacks if not isinstance(c, TQDMProgressBar)]
 
     def teardown(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:
         delattr(self, "_predict")
 
-    def evaluate_epoch(
-            self,
-            trainer: Trainer,
-            stage: str,
-            loaders: Sequence[Loader]
-    ) -> None:
+    def evaluate_epoch(self, trainer: Trainer, stage: str, loaders: Sequence[Loader]) -> None:
         for dataloader_idx, (ids, load_x, load_y) in enumerate(loaders):
             for idx, x, y in zip_equal(ids, map(load_x, ids), map(load_y, ids)):
-                _call_callback_hooks(trainer, f"on_{stage}_batch_start", (x[None, ...], y[None, ...]), idx,
-                                     dataloader_idx)
+                args = ((x[None, ...], y[None, ...]), idx, dataloader_idx)
+
+                hook_name = f"on_{stage}_batch_start"
+                _call_callback_hooks(trainer, hook_name, *args)
+                _call_lightning_module_hook(trainer, hook_name, *args)
+
                 predict = self._predict(x)
-                _call_callback_hooks(
-                    trainer,
-                    f"on_{stage}_batch_end",
-                    (predict[None, ...], y[None, ...]),
-                    (x[None, ...], y[None, ...]),
-                    idx,
-                    dataloader_idx,
-                )
+
+                hook_name = f"on_{stage}_batch_end"
+                _call_callback_hooks(trainer, hook_name, (predict[None, ...], y[None, ...]), *args)
+                _call_lightning_module_hook(trainer, hook_name, (predict[None, ...], y[None, ...]), *args)
 
     def on_validation_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         self.evaluate_epoch(trainer, "validation", self.val_loaders)
