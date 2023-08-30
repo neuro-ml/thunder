3be1049aa2c771c8c0078b1ee33200213b50a933
arseniybelkov <arsfizteh@gmail.com>
2023-08-30 17:25:02 +0300

    docs for thunder.torch.utils

---
+++ docs/utils.md
@@ -0,0 +1,7 @@
+# Utils
+Some useful utility functions.
+::: thunder.torch.utils
+    handler: python
+    options:
+      show_root_heading: true
+      show_source: true
--- thunder/torch/utils.py
+++ thunder/torch/utils.py
@@ -10,6 +10,15 @@ from ..utils import squeeze_first
 
 
 def get_device(x: Union[torch.Tensor, nn.Module]) -> torch.device:
+    """
+    Infer device of torch.Tensor or nn.Module instance.
+    Parameters
+    ----------
+    x: Union[torch.Tensor, nn.Module]
+    Returns
+    -------
+    device: torch.device
+    """
     if isinstance(x, (torch.Tensor, LightningModule)):
         return x.device
     elif isinstance(x, nn.Module):
@@ -22,14 +31,57 @@ def get_device(x: Union[torch.Tensor, nn.Module]) -> torch.device:
 
 
 def to_np(*x: Any) -> Any:
+    """
+    Converts collection of tensors into numpy arrays.
+    Parameters
+    ----------
+    *x: Any
+    Returns
+    ----------
+    Collection of numpy arrays
+    Examples
+    --------
+    >>> x, y  # torch.Tensor
+    >>> z = to_np(x) # convert to numpy array
+    >>> x, y = to_np(x, y) # x and y are now numpy arrays
+    >>> x, y, z = to_np(x, y, z) # to_np converts only tensors and does not affect other types
+    >>> dict_of_np = to_np(dict_of_tensors) # to_np converts any collection
+    """
     return squeeze_first(apply_to_collection(x, torch.Tensor, tensor2np))
 
 
 def tensor2np(x: torch.Tensor) -> np.ndarray:
+    """
+    Detaches, moves torch.Tensor to CPU and converts into numpy array.
+    Parameters
+    ----------
+    x: torch.Tensor
+    Returns
+    -------
+    np.ndarray
+    """
     return x.detach().cpu().numpy()
 
 
 def maybe_from_np(*x: Any, device: Union[torch.device, str] = "cpu") -> Any:
+    """
+    Recursively converts numpy arrays to torch.Tensor.
+    Parameters
+    ----------
+    *x: Any
+    device: Union[torch.device, str]
+        Device to move to, default is CPU.
+    Returns
+    -------
+    Collection of tensors.
+    Examples
+    -------
+    >>> x, y  # np.ndarray
+    >>> z = maybe_from_np(x) # convert to torch.Tensor
+    >>> x, y = maybe_from_np(x, y) # x and y are now tensors
+    >>> x, y, z = maybe_from_np(x, y, z) # maybe_from_np converts np arrays and tensors and does not affect other types
+    >>> dict_of_tensors = to_np(dict_of_np) # maybe_from_np converts any collection
+    """
     def to_tensor(x):
         if isinstance(x, torch.Tensor):
             return x.to(device)
