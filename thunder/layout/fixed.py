5b0f7e86f2b033f8967f45002ad3e8c8211b0dd3
Arseniy Belkov <arseniybelkov@yandex.ru>
2023-10-12 18:38:39 +0300

    micro commit: add a few error checks

--- thunder/layout/fixed.py
+++ thunder/layout/fixed.py
@@ -18,9 +18,10 @@ class FixedSplit(Split):
             Names of folds, e.g. 'train', 'val', test'.
         """
         if names is not None:
-            # TODO
-            assert len(set(names)) == len(names)
-            assert len(splits[0]) == len(names)
+            if len(set(names)) != len(names):
+                raise ValueError(f"Names of splits are not unique: {names}")
+            if len(splits[0]) != len(names):
+                raise ValueError(f"Got {len(splits[0])} and {len(names)} fold names: {names}")
 
         self.entries = sorted(set(collapse(splits)))
         self.splits = [tuple(fold) for fold in splits]
@@ -40,10 +41,12 @@ class FixedSingleSplit(SingleSplit):
         Names of folds, e.g. 'train', 'val', test'. If data is of type `dict`,
         then it is not required.
         """
-        self.entries = sorted(set(collapse(split)))
-
         if isinstance(split, dict):
             names = split.keys()  # from python3.7 order is guaranteed.
             split = split.values()
+
+        if len(names) != len(split):
+            raise ValueError("Difference in number of splits and number of names: "
+                             f"{len(split)} and {len(names)}")
 
         self.split = dict(zip_equal(names, split))
