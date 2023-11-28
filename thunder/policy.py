7f5e128f372342ca47204a1698184932eb2842ff
Arseniy Belkov <arseniybelkov@yandex.ru>
2023-11-29 00:58:52 +0300

    fix `Schedule` load_state_dict

--- thunder/policy.py
+++ thunder/policy.py
@@ -163,7 +163,7 @@ class Schedule(MappingPolicy):
         return juxt(self.current_mapping)(self.last_epoch)
 
     def state_dict(self) -> Dict[str, Any]:
-        return super().state_dict("mapping")
+        return super().state_dict("mapping", "current_mapping")
 
     def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
         super().load_state_dict(state_dict)
