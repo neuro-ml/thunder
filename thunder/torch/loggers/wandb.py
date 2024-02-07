05343038f105c143a34dfc3cadbd1225f89964d7
Arseniy Belkov <arseniybelkov@yandex.ru>
2024-02-07 17:38:56 +0300

    isort

--- thunder/torch/loggers/wandb.py
+++ thunder/torch/loggers/wandb.py
@@ -1,8 +1,8 @@
+import os
+from pathlib import Path
 from typing import Union
 
-from pathlib import Path
 import wandb
-import os
 from lightning.pytorch.loggers import WandbLogger as _WandbLogger
 
 
