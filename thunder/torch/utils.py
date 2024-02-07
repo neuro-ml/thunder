5f65fb1ad1c2e672b49c63ce669335a6c58e1a6a
Arseniy Belkov <arseniybelkov@yandex.ru>
2024-02-07 19:44:27 +0300

    no antipatterns !!!

--- thunder/torch/utils.py
+++ thunder/torch/utils.py
@@ -103,8 +103,7 @@ def last_checkpoint(root: Union[Path, str]) -> Union[Path, str]:
     checkpoint_path: Union[Path, str]
         If last.ckpt exists - returns Path to it. Otherwise, returns 'last'.
     """
-    glob = Path(root).glob("**/*.ckpt")
-    checkpoints = list(filter(lambda p: p.name != "last.ckpt", glob))
+    checkpoints = [p for p in Path(root).glob("**/*.ckpt") if p.name != "last.ckpt"]
     if not checkpoints:
         return "last"
     return max(checkpoints, key=lambda t: os.stat(t).st_mtime)
