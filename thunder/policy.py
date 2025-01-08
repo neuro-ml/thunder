cf530340dfcf5535876b30ed3f6e4b5d2b2b84c4
Arseniy Belkov <arsfizteh@gmail.com>
2025-01-08 22:34:31 +0300

    remove redundant lines

--- thunder/policy.py
+++ thunder/policy.py
@@ -13,8 +13,6 @@ try:
     from torch.optim.lr_scheduler import LRScheduler
 except ImportError:
     from torch.optim.lr_scheduler import _LRScheduler as LRScheduler
-except Exception:
-    raise
 
 
 class Policy(LRScheduler, metaclass=ABCMeta):
